da941e66cb3ecca4e5e1e8397742360d1bb3873c
Kyle Ines <kyleines@gmail.com>
2021-03-23 18:15:31 -0500

    Hide 'other job' text field until selected

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 document.querySelector("#name").focus();
+
+const otherJob = document.querySelector("#other-job-role");
+otherJob.style.display = "none";
+const jobTitle = document.querySelector("#title");
+jobTitle.addEventListener("change", (e) => {
+    if (e.target.value === "other") {
+        otherJob.style.display = "";
+    } else {
+        otherJob.style.display = "none";
+    }
+});
