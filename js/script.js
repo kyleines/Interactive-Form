409a0c2766b5b7d0f71d45ea428b4aae8c630dab
Kyle Ines <kyleines@gmail.com>
2021-03-23 19:30:47 -0500

    Add functionality to 'T-shirt Info' section

--- js/script.js
+++ js/script.js
@@ -10,3 +10,24 @@ jobTitle.addEventListener("change", (e) => {
         otherJob.style.display = "none";
     }
 });
+
+const shirtDesign = document.querySelector("#design");
+const shirtColor = document.querySelector("#color");
+shirtColor.disabled = true;
+shirtDesign.addEventListener("change", (e) => {
+    shirtColor.disabled = false;
+    for (let i = 0; i < shirtColor.length; i++) {
+        shirtColor.options[i].hidden = true;
+        if (e.target.value === "js puns") {
+            shirtColor.options[1].selected = true;
+            shirtColor.options[1].hidden = false;
+            shirtColor.options[2].hidden = false;
+            shirtColor.options[3].hidden = false;
+        } else {
+            shirtColor.options[4].selected = true;
+            shirtColor.options[4].hidden = false;
+            shirtColor.options[5].hidden = false;
+            shirtColor.options[6].hidden = false;
+        }
+    }
+});
