79cee0102c99e8a9554294d6a4bd094b568ced01
Kyle Ines <kyleines@gmail.com>
2021-03-24 15:29:19 -0500

    Add functionality to 'Register Activities' section

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ jobTitle.addEventListener("change", (e) => {
     }
 });
 
+
 const shirtDesign = document.querySelector("#design");
 const shirtColor = document.querySelector("#color");
 shirtColor.disabled = true;
@@ -30,4 +31,20 @@ shirtDesign.addEventListener("change", (e) => {
             shirtColor.options[6].hidden = false;
         }
     }
+});
+
+
+const activities = document.querySelector("#activities");
+const checkboxes = document.querySelectorAll("#activities input");
+const totalCost = document.querySelector("#activities-cost");
+activities.addEventListener("change", (e) => {
+    const clicked = e.target;
+    const checkedCost = clicked.getAttribute("data-cost");
+    let newTotal = 0;
+    for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            newTotal += parseInt(checkboxes[i].getAttribute("data-cost"));
+        }
+    }
+    totalCost.textContent = `Total: $${newTotal}`;
 });
